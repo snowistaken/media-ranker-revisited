d6f1e7442fbba99710c0af1615c4bcd024a85a6e
Emily Cowan <e.anderson8977@gmail.com>
2020-06-21 20:35:58 -0700

    Implemented OAuth login/logout functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_04_07_164321) do
+ActiveRecord::Schema.define(version: 2020_06_22_032843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2017_04_07_164321) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
+    t.string "provider"
   end
 
   create_table "votes", id: :serial, force: :cascade do |t|
